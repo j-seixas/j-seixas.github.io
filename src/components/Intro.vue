2cf7158ba2a7220f3ebede48a8db0b3e35a4e203
João Seixas <jnuno1997@gmail.com>
2020-06-14 17:13:14 +0100

    Add social icons

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,13 +5,35 @@
                 <h1 class="title name">
                     João Seixas
                 </h1>
-                <h2 class="subtitle is-4">
-                    <p>Student in Faculty of Engineering, University of Porto in Portugal.</p>
-                    <p>Taking BSc (3 years) + MSc (2 years) in Informatics and Computer Engineering (currently on the 5th and last year).</p>
+                <h2 class="subtitle">
+                    <p><a href="https://sigarra.up.pt/feup/en/CUR_GERAL.CUR_VIEW?pv_curso_id=742"> BSc + MSc Informatics and Computer Engineering</a> Student <a href="https://sigarra.up.pt/feup/pt/web_page.inicial">@FEUP</a></p>
                 </h2>
-                <section>
-                    <a v-bind:href='pdfLink' download class="button is-custom-y1 is-light">CV</a>
-                </section>
+                <div class="container social">
+                    <span class="icon is-medium">
+                        <a href="mailto:jnuno1997@gmail.com" class="icon-link">
+                            <i class="fas fa-2x fa-envelope"></i>
+                        </a>
+                    </span>
+                    <span class="icon is-medium">
+                        <a href="https://github.com/j-seixas" class="icon-link">
+                            <i class="fab fa-2x fa-github"></i>
+                        </a>
+                    </span>
+                    <span class="icon is-medium">
+                        <a href="https://linkedin.com/in/joao-seixas/" class="icon-link">
+                            <i class="fab fa-2x fa-linkedin"></i>
+                        </a>
+                    </span>
+                        <span class="icon is-medium resume">
+                            <a v-bind:href='pdfLink' download class="icon-link">
+                                <i class="fas fa-2x fa-file-download"></i>
+                                
+                            </a>
+                        </span>
+                        <span class="cv">
+                            <b> Curriculum </b>
+                        </span>
+                </div>
             </div>
         </div>
     </section>
@@ -32,10 +54,48 @@ export default {
 <style lang="scss" scoped>
     @import '../styles/global';
 
+    $visible: none;
+
     h1.name {
         font-size: 4rem;
     }
+
+    h2.subtitle {
+        a {
+            color: $text;
+            &:hover {
+                color: $custom-y1-dark
+            }
+        }
+    }
+
+    div.container.social {
+        display: inline-flex;
+
+        span.icon.is-medium {
+            margin-right: 1em;
+
+            a:hover {
+                color: $custom-y1-dark
+            }
+        }
+        
+        span.icon.is-medium.resume {
+            margin-right: 0;
+        }
+
+        span.resume:hover + span.cv{
+            visibility: visible;
+        }
+
+        span.cv {
+
+            visibility: hidden;
+        }
+    }
     
+
+
     a.is-custom-y1.is-light {
         box-shadow: 0 0.5em 1em -0.125em rgba($scheme-invert, 0.1), 0 0px 0 1px rgba($scheme-invert, 0.02);
     }
