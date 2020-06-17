37acf3d05a4c3ba901fd982242d26015501f0b15
João Seixas <jnuno1997@gmail.com>
2020-06-17 15:08:18 +0100

    title in webpack plugin options

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = {
     config.module.rule('pdf')
       .test(/\.pdf$/)
       .use('file-loader').loader('file-loader')
+
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'João Seixas, Informatics and Computing Engineering Student'
+        return args
+      })
   }
 }
